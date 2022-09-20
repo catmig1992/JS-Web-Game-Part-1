9d7ea132d5103041b688250db2cf385106b8ece1
Catherine Miguel <catmig1992@gmail.com>
2022-09-19 20:47:37 -0700

    step 5 - second half: add sword img via refactor function

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ function newImage(url, left, bottom) {
   return imageInput;
 }
 
+function newItem(url, left, bottom) {
+  let imageInput = newImage(url, left, bottom);
+
+  imageInput.addEventListener("click", function () {
+    imageInput.remove();
+  });
+}
+
 newImage("assets/green-character.gif", 100, 100);
 newImage("assets/pine-tree.png", 450, 200);
 newImage("assets/tree.png", 200, 300);
@@ -15,13 +23,4 @@ newImage("assets/pillar.png", 350, 100);
 newImage("assets/crate.png", 150, 200);
 newImage("assets/well.png", 500, 425);
 
-let sword = document.createElement("img");
-sword.src = "assets/sword.png";
-sword.position = "fixed";
-sword.left = "500px";
-sword.bottom = "405px";
-document.body.append(sword);
-
-sword.addEventListener("click", function () {
-  sword.remove();
-});
+newItem("assets/sword.png", 500, 405);
